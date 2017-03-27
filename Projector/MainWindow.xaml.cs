0fed29d155f61d7181ffd44535ecb4c76d8528fc
kenpark93 <ANInkin@mail.ru>
2017-03-27 21:49:46 +0300

    27.03

--- Projector/MainWindow.xaml.cs
+++ Projector/MainWindow.xaml.cs
@@ -20,9 +20,11 @@ namespace Projector
     /// </summary>
     public partial class MainWindow : Window
     {
+
+
         public MainWindow()
         {
-            InitializeComponent(); 
+            InitializeComponent();
         }
 
         private void MenuItem_Click(object sender, RoutedEventArgs e)
